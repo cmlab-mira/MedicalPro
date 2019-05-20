cd8fc6d711efdc77f2da9d2551109950fd3afbb4
peter850706 <peter850706@gmail.com>
2019-05-20 17:04:25 +0800

    Implement normalize

--- src/data/transformers.py
+++ src/data/transformers.py
@@ -58,3 +58,48 @@ class ToTensor:
         else:
             raise ValueError("All of the images' dimensions should be 3 (2D images) or 4 (3D images).")
         return imgs
+
+
+class Normalize:
+    """Normalize a tuple of images with mean and standard deviation.
+    Args:
+        means (int or list): A sequence of means for each channel.
+        stds (int or list): A sequence of standard deviations for each channel.
+    """
+    def __init__(self, means, stds):
+        if means is not None and stds is not None:
+            means = tuple(means)
+            stds = tuple(stds)
+        self.means = means
+        self.stds = stds
+
+    def __call__(self, *imgs):
+        """
+        Args:
+            imgs (tuple of numpy.ndarray): The images to be normalized.
+
+        Returns:
+            imgs (tuple of numpy.ndarray): The normalized images.
+        """
+        if not all(isinstance(img, np.ndarray) for img in imgs):
+            raise TypeError('All of the images should be numpy.ndarray.')
+
+        # Apply image-level normalization.
+        if self.means is None and self.stds is None:
+            _imgs = []
+            for img in imgs:
+                axis = tuple(range(img.ndim - 1))
+                means = img.mean(axis=axis)
+                stds = img.std(axis=axis)
+                img = self.normalize(img, means, stds)
+                _imgs.append(img)
+            imgs = tuple(_imgs)
+        else:
+            imgs = map(functools.partial(self.normalize, means=self.means, stds=self.stds), imgs)
+        return imgs
+
+    @staticmethod
+    def normalize(img, means, stds):
+        for c, mean, std in zip(range(img.shape[-1]), means, stds):
+            img[..., c] = (img[..., c] - mean) / std
+        return img

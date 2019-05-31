46cbda7a04dc91550cea011757f0ae8469325105
peter850706 <peter850706@gmail.com>
2019-05-31 22:23:57 +0800

    Implement RandomElasticDeformation

--- src/data/transformers.py
+++ src/data/transformers.py
@@ -207,3 +207,98 @@ class RandomCrop(BaseTransformer):
             ht, wt, dt = size
             h0, w0, d0 = random.randint(0, h - ht), random.randint(0, w - wt), random.randint(0, d - dt)
             return h0, h0 + ht, w0, w0 + wt, d0, d0 + dt
+
+
+class RandomElasticDeformation(BaseTransformer):
+    """Do the random elastic deformation as used in U-Net and V-Net by using the bspline transform.
+    Args:
+        do_z_deformation (bool): Whether to apply the deformation along the z dimension (default: False).
+        num_ctrl_points (int): The number of the control points to form the control point grid (default: 4).
+        sigma (int or float): The number to determine the extent of deformation (default: 15).
+        prob (float): The probability of applying the deformation (default: 0.5).
+    """
+    def __init__(self, do_z_deformation=False, num_ctrl_points=4, sigma=15, prob=0.5):
+        self.do_z_deformation = do_z_deformation
+        self.num_ctrl_points = max(num_ctrl_points, 2)
+        self.sigma = max(sigma, 1)
+        self.prob = max(0, min(prob, 1))
+        self.bspline_transform = None
+
+    def __call__(self, *imgs, elastic_deformation_orders=None, **kwargs):
+        """
+        Args:
+            imgs (tuple of numpy.ndarray): The images to be deformed.
+            elastic_deformation_orders (sequence of int): The corresponding interpolation order of the images (default: None, the interpolation order would be 3 for all the images).
+
+        Returns:
+            imgs (tuple of numpy.ndarray): The deformed images.
+        """
+        if not all(isinstance(img, np.ndarray) for img in imgs):
+            raise TypeError('All of the images should be numpy.ndarray.')
+
+        if not all(img.ndim == 3 for img in imgs) and not all(img.ndim == 4 for img in imgs):
+            raise ValueError("All of the images' dimensions should be 3 (2D images) or 4 (3D images).")
+
+        if random.random() < self.prob:
+            self._init_bspline_transform(imgs[0].shape)
+            if elastic_deformation_orders:
+                _imgs = []
+                for img, order in zip(imgs, elastic_deformation_orders):
+                    img = self._apply_bspline_transform(img, order)
+                    _imgs.append(img)
+                imgs = tuple(_imgs)
+            else:
+                imgs = map(self._apply_bspline_transform, imgs)
+        return imgs
+
+    def _init_bspline_transform(self, shape):
+        """Initialize the bspline transform.
+        Args:
+            shape (tuple): The size of the control point grid.
+        """
+        # Remove the channel dimension.
+        shape = shape[:-1]
+
+        # Initialize the control point grid.
+        img = sitk.GetImageFromArray(np.zeros(shape))
+        mesh_size = [self.num_ctrl_points] * img.GetDimension()
+        self.bspline_transform = sitk.BSplineTransformInitializer(img, mesh_size)
+
+        # Set the parameters of the bspline transform randomly.
+        params = self.bspline_transform.GetParameters()
+        params = np.asarray(params, dtype=np.float64)
+        params = params + np.random.randn(params.shape[0]) * self.sigma
+        if len(shape) == 3 and not self.do_z_deformation:
+            params[0: len(params) // 3] = 0
+        params = tuple(params)
+        self.bspline_transform.SetParameters(params)
+
+    def _apply_bspline_transform(img, order=3):
+        """Apply the bspline transform.
+        Args:
+            img (np.ndarray): The image to be deformed.
+            order (int): The interpolation order (default: 3, should be 0, 1 or 3).
+
+        Returns:
+            img (np.ndarray): The deformed image.
+        """
+        # Create the resampler.
+        resampler = sitk.ResampleImageFilter()
+        if order == 0:
+            resampler.SetInterpolator(sitk.sitkNearestNeighbor)
+        elif order == 1:
+            resampler.SetInterpolator(sitk.sitkLinear)
+        elif order == 3:
+            resampler.SetInterpolator(sitk.sitkBSpline)
+        else:
+            raise ValueError(f'The interpolation order should be 0, 1 or 3. Got {order}.')
+
+        # Apply the bspline transform.
+        shape = img.shape
+        img = sitk.GetImageFromArray(np.squeeze(img))
+        resampler.SetReferenceImage(img)
+        resampler.SetDefaultPixelValue(0)
+        resampler.SetTransform(self.bspline_transform)
+        img = resampler.Execute(img)
+        img = sitk.GetArrayFromImage(img).reshape(shape)
+        return img

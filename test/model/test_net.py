59ad3ed91c36b33e14932bcbbfbf26be24730fb7
vic85821 <vic85821@gmail.com>
2019-06-03 21:11:45 +0800

    Add the base_logger unit test

--- test/model/test_net.py
+++ test/model/test_net.py
@@ -2,6 +2,7 @@ import torch.nn as nn
 import numpy as np
 from box import Box
 from pathlib import Path
+
 from src.model.nets.base_net import BaseNet
 
 
@@ -12,6 +13,11 @@ class MyNet(BaseNet):
         self.out_channels = out_channels
         self.conv1 = nn.Conv2d(self.in_channels, 10, kernel_size=3)
         self.conv2 = nn.Conv2d(10, self.out_channels, kernel_size=3)
+    
+    def forward(self, x):
+        conv1 = self.conv1(x)
+        output = self.conv2(conv1)
+        return output
 
 
 def test_base_net():

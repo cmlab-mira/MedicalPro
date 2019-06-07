3600393a4cb34da68b6d294d641dded5cd457efc
vic85821 <vic85821@gmail.com>
2019-06-07 18:19:22 +0800

    Remove the initialization of the tranform in the base_dataset

--- src/data/datasets/base_dataset.py
+++ src/data/datasets/base_dataset.py
@@ -1,7 +1,6 @@
 import torch
 from torch.utils.data import Dataset
 from pathlib import Path
-from src.data.transformers import compose
 
 
 class BaseDataset(Dataset):
@@ -9,10 +8,8 @@ class BaseDataset(Dataset):
     Args:
         type (str): The type of the dataset including train, valid and test.
         data_root (str): The root directory of the saved data.
-        transforms (Box): The preprocessing and augmentation techniques applied to the data (default: None).
     """
     def __init__(self, type, data_root, transforms=None):
         super().__init__()
         self.type = type
         self.data_root = Path(data_root)
-        self.transforms = compose(transforms)

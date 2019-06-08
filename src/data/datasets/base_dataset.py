dfbe356ae6c7052d82bc827968d2deb5ca7fd93f
peter850706 <peter850706@gmail.com>
2019-06-08 18:39:09 +0800

    Modify BaseDataset: delete unused arg and update comments

--- src/data/datasets/base_dataset.py
+++ src/data/datasets/base_dataset.py
@@ -6,10 +6,10 @@ from pathlib import Path
 class BaseDataset(Dataset):
     """The base class for all datasets.
     Args:
-        type (str): The type of the dataset including train, valid and test.
+        type (str): The type of the dataset ('train', 'valid' or 'test').
         data_root (str): The root directory of the saved data.
     """
-    def __init__(self, type, data_root, transforms=None):
+    def __init__(self, type, data_root):
         super().__init__()
         self.type = type
         self.data_root = Path(data_root)

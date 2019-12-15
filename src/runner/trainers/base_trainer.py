c087b1e2ec1139a78c6ee286c581c0aa80c1e260
peter850706 <peter850706@gmail.com>
2019-12-15 11:33:29 +0800

    Support gradient accumulation

--- src/runner/trainers/base_trainer.py
+++ src/runner/trainers/base_trainer.py
@@ -27,11 +27,13 @@ class BaseTrainer:
         monitor (Monitor): The object to determine whether to save the checkpoint.
         num_epochs (int): The total number of training epochs.
         valid_freq (int): The validation frequency (default: 1).
+        grad_accumulation_steps (int): The number of gradient accumulation steps (default: 1).
         opt_level (str): The optimization level of apex.amp (default: 'O0').
     """
     def __init__(self, device, train_dataloader, valid_dataloader,
                  net, loss_fns, loss_weights, metric_fns, optimizer,
-                 lr_scheduler, logger, monitor, num_epochs, valid_freq=1, opt_level='O0'):
+                 lr_scheduler, logger, monitor, num_epochs,
+                 valid_freq=1, grad_accumulation_steps=1, opt_level='O0'):
         self.device = device
         self.train_dataloader = train_dataloader
         self.valid_dataloader = valid_dataloader
@@ -42,15 +44,15 @@ class BaseTrainer:
         self.optimizer = optimizer
         if APEX_AVAILABLE:
             self.net, self.optimizer = amp.initialize(self.net, self.optimizer, opt_level=opt_level)
-
         if isinstance(self.lr_scheduler, torch.optim.lr_scheduler.ReduceLROnPlateau):
             raise NotImplementedError('Do not support torch.optim.lr_scheduler.ReduceLROnPlateau scheduler yet.')
         self.lr_scheduler = lr_scheduler
-
         self.logger = logger
         self.monitor = monitor
         self.num_epochs = num_epochs
         self.valid_freq = valid_freq
+        self.grad_accumulation_steps = GradAccumulationSteps(grad_accumulation_steps=grad_accumulation_steps,
+                                                             total_steps=len(train_dataloader))
         self.epoch = 1
         self.np_random_seeds = None
 
@@ -88,7 +90,7 @@ class BaseTrainer:
             if saved_path:
                 logging.info(f'Save the checkpoint to {saved_path}.')
                 self.save(saved_path)
-            
+
             if self.epoch % self.valid_freq == 0:
                 # Save the best checkpoint.
                 saved_path = self.monitor.is_best(valid_log)
@@ -122,26 +124,29 @@ class BaseTrainer:
         else:
             self.net.eval()
         dataloader = self.train_dataloader if mode == 'training' else self.valid_dataloader
-        trange = tqdm(dataloader,
+        trange = tqdm(enumerate(dataloader),
                       total=len(dataloader),
                       desc=mode)
 
         log = self._init_log()
         count = 0
-        for batch in trange:
+        for i, batch in trange:
             batch = self._allocate_data(batch)
             inputs, targets = self._get_inputs_targets(batch)
             if mode == 'training':
                 outputs = self.net(inputs)
                 losses = self._compute_losses(outputs, targets)
                 loss = (torch.stack(losses) * self.loss_weights).sum()
-                self.optimizer.zero_grad()
                 if APEX_AVAILABLE:
                     with amp.scale_loss(loss, self.optimizer) as scaled_loss:
+                        scaled_loss /= self.grad_accumulation_steps(i)
                         scaled_loss.backward()
                 else:
+                    loss /= self.grad_accumulation_steps(i)
                     loss.backward()
-                self.optimizer.step()
+                if (i + 1) % self.grad_accumulate_steps() == 0 or (i + 1) == len(dataloader):
+                    self.optimizer.step()
+                    self.optimizer.zero_grad()
                 if isinstance(self.lr_scheduler, torch.optim.lr_scheduler.CyclicLR):
                     self.lr_scheduler.step()
                 with torch.no_grad():
@@ -272,3 +277,20 @@ class BaseTrainer:
         self.np_random_seeds = checkpoint['np_random_seeds']
         if checkpoint['amp']:
             amp.load_state_dict(checkpoint['amp'])
+
+
+class GradAccumulationSteps:
+    """The gradient accumulation which considers the last training steps in an epoch.
+    Args:
+        grad_accumulation_steps (int): The number of gradient accumulation steps.
+        total_steps (int): The total number of training steps in an epoch.
+    """
+    def __init__(self, grad_accumulation_steps, total_steps):
+        self.grad_accumulation_steps = grad_accumulation_steps
+        self.last_steps = list(range(total_steps - total_steps % grad_accumulation_steps, total_steps))
+
+    def __call__(self, i=None):
+        if i in self.last_steps:
+            return len(self.last_steps)
+        else:
+            return self.grad_accumulation_steps

e81ccb8eee22d3969d8ab1991d241c5e81d1abc8
peter850706 <peter850706@gmail.com>
2020-02-02 15:32:41 +0800

    Delete UNet3D

--- src/model/nets/__init__.py
+++ src/model/nets/__init__.py
@@ -1,10 +1,8 @@
 from .base_net import BaseNet
-from .unet3d import UNet3D
 from .models_genesis import ModelsGenesisSegNet, ModelsGenesisClfNet
 
 __all__ = [
     'BaseNet',
-    'UNet3D',
     'ModelsGenesisSegNet',
     'ModelsGenesisClfNet',
 ]

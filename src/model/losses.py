3704764fc582a319254d2a945d3280a834d094b6
peter850706 <peter850706@gmail.com>
2020-03-27 20:23:54 +0800

    Rewrite the losses.py

--- src/model/losses.py
+++ src/model/losses.py
@@ -22,7 +22,7 @@ class BCELossWrapper(nn.Module):
         with_logits (bool, optional): Specify the output is logits or probability (default: True).
         weight (sequence, optional): The same argument in torch.nn.BCEWithLogitsLoss and torch.nn.BCELoss
             but its type is sequence for the configuration purpose (default: None).
-        pos_weight (sequence, optional): The same argument in torch.nn.BCEWithLogitsLoss and torch.nn.BCELoss
+        pos_weight (sequence, optional): The same argument in torch.nn.BCEWithLogitsLoss
             but its type is sequence for the configuration purpose (default: None).
     """
 
@@ -30,10 +30,10 @@ class BCELossWrapper(nn.Module):
         super().__init__()
         if weight is not None:
             weight = torch.tensor(weight, dtype=torch.float)
-            kwargs.update({'weight': weight})
+            kwargs.update(weight=weight)
         if pos_weight is not None:
             pos_weight = torch.tensor(pos_weight, dtype=torch.float)
-            kwargs.update({'pos_weight': pos_weight})
+            kwargs.update(pos_weight=pos_weight)
         self.loss_fn = (nn.BCEWithLogitsLoss if with_logits else nn.BCELoss)(**kwargs)
 
     def forward(self, output, target):
@@ -66,7 +66,7 @@ class CrossEntropyLossWrapper(nn.Module):
         super().__init__()
         if weight is not None:
             weight = torch.tensor(weight, dtype=torch.float)
-            kwargs.update({'weight': weight})
+            kwargs.update(weight=weight)
         self.loss_fn = (nn.CrossEntropyLoss if with_logits else nn.NLLLoss)(**kwargs)
 
     def forward(self, output, target):
@@ -84,27 +84,24 @@ class CrossEntropyLossWrapper(nn.Module):
 class DiceLoss(nn.Module):
     """The Dice loss.
     Refs:
+        https://arxiv.org/pdf/1606.04797
         https://github.com/pytorch/pytorch/issues/1249#issuecomment-337999895
 
     Args:
         binary (bool, optional): Whether to use the binary mode, which expects the task is
             binary classification, the activate function is sigmoid and output channel is 1
-            (similar to torch.nn.BCEWithLogitsLoss or torch.nn.BCELoss) (default: False).
+            (similar to torch.nn.BCEWithLogitsLoss and torch.nn.BCELoss) (default: False).
         with_logits (bool, optional): Specify the output is logits or probability (default: True).
         smooth (float, optional): The smooth term (default: 1.0).
         square (bool, optional): Whether to use the square of cardinality (default: True).
-        weight (sequence, optional): The weight given to each class (default: None).
     """
 
-    def __init__(self, binary=False, with_logits=True, smooth=1.0, square=True, weight=None):
+    def __init__(self, binary=False, with_logits=True, smooth=1.0, square=True):
         super().__init__()
         self.binary = binary
         self.with_logits = with_logits
         self.smooth = smooth
         self.square = square
-        if weight is not None:
-            weight = torch.tensor(weight, dtype=torch.float)
-            self.register_buffer('weight', weight)
 
     def forward(self, output, target):
         """
@@ -129,11 +126,7 @@ class DiceLoss(nn.Module):
             cardinality = (output ** 2).sum(reduced_dims) + (target ** 2).sum(reduced_dims)
         else:
             cardinality = output.sum(reduced_dims) + target.sum(reduced_dims)
-        loss = 1 - (2 * intersection + self.smooth) / (cardinality + self.smooth)
-        if hasattr(self, 'weight'):
-            loss = (self.weight * loss).sum(dim=1).mean()
-        else:
-            loss = loss.mean()
+        loss = (1 - (2 * intersection + self.smooth) / (cardinality + self.smooth)).mean()
         return loss
 
 
@@ -145,24 +138,20 @@ class TverskyLoss(nn.Module):
     Args:
         binary (bool, optional): Whether to use the binary mode, which expects the task is
             binary classification, the activate function is sigmoid and output channel is 1
-            (similar to torch.nn.BCEWithLogitsLoss or torch.nn.BCELoss) (default: False).
+            (similar to torch.nn.BCEWithLogitsLoss and torch.nn.BCELoss) (default: False).
         with_logits (bool, optional): Specify the output is logits or probability (default: True).
         alpha (float, optional): The magnitude of penalties for the False Positives (FPs) (default: 0.5).
         beta (float, optional): The magnitude of penalties for the False Negatives (FNs) (default: 0.5).
         smooth (float, optional): The smooth term (default: 1.0).
-        weight (sequence, optional): The weight given to each class (default: None).
     """
 
-    def __init__(self, binary=False, with_logits=True, alpha=0.5, beta=0.5, smooth=1.0, weight=None):
+    def __init__(self, binary=False, with_logits=True, alpha=0.5, beta=0.5, smooth=1.0):
         super().__init__()
         self.binary = binary
         self.with_logits = with_logits
         self.alpha = alpha
         self.beta = beta
         self.smooth = smooth
-        if weight is not None:
-            weight = torch.tensor(weight, dtype=torch.float)
-            self.register_buffer('weight', weight)
 
     def forward(self, output, target):
         """
@@ -185,9 +174,7 @@ class TverskyLoss(nn.Module):
         intersection = (output * target).sum(reduced_dims)
         fps = (output * (1 - target)).sum(reduced_dims)
         fns = ((1 - output) * target).sum(reduced_dims)
-        loss = 1 - (intersection + self.smooth) / (intersection + self.alpha * fps + self.beta * fns + self.smooth)
-        if hasattr(self, 'weight'):
-            loss = (self.weight * loss).sum(dim=1).mean()
-        else:
-            loss = loss.mean()
+        loss = (
+            1 - (intersection + self.smooth) / (intersection + self.alpha * fps + self.beta * fns + self.smooth)
+        ).mean()
         return loss

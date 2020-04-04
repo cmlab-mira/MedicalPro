0cc6ffd958f6e879cf7a248289db8f0371e01f31
peter850706 <peter850706@gmail.com>
2020-04-04 15:07:29 +0800

    Implement AcdcAdaptTrainer

--- src/runner/trainers/__init__.py
+++ src/runner/trainers/__init__.py
@@ -1,7 +1,9 @@
 from .base_trainer import BaseTrainer
 from .acdc_seg_trainer import AcdcSegTrainer
+from .acdc_adapt_trainer import AcdcAdaptTrainer
 
 __all__ = [
     'BaseTrainer',
     'AcdcSegTrainer',
+    'AcdcAdaptTrainer',
 ]

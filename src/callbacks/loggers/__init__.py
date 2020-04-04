9e3b403a013ed9f3b50360b1a99f70db9fb293f7
peter850706 <peter850706@gmail.com>
2020-04-04 15:08:10 +0800

    Implement AcdcAdaptLogger

--- src/callbacks/loggers/__init__.py
+++ src/callbacks/loggers/__init__.py
@@ -1,7 +1,9 @@
 from .base_logger import BaseLogger
 from .acdc_seg_logger import AcdcSegLogger
+from .acdc_adapt_logger import AcdcAdaptLogger
 
 __all__ = [
     'BaseLogger',
     'AcdcSegLogger',
+    'AcdcAdaptLogger',
 ]

bc828f7d1ae0abec9ab7c4cd54cb6284dd547115
vic85821 <vic85821@gmail.com>
2020-05-27 20:16:16 +0800

    Update the lits data split script

--- src/preprocess/lits_data_split.py
+++ src/preprocess/lits_data_split.py
@@ -8,32 +8,31 @@ from pathlib import Path
 def main(args):
     # Randomly split the data into k folds.
     patient_dirs = sorted(dir_ for dir_ in (args.data_dir / 'training').iterdir() if dir_.is_dir())
-    random.seed(4)
+    random.seed(3)
     folds = random.sample(patient_dirs, k=len(patient_dirs))
 
     output_dir = args.output_dir
     if not output_dir.is_dir():
         output_dir.mkdir(parents=True)
-    ratio = len(folds) // (args.k + 1)
-    test_start = args.k * ratio
-    test_folds = folds[test_start:]
+    test_size = args.test_size
+    test_folds = folds[-test_size:]
+    ratio = (len(folds) - test_size) // args.k
     for i in range(args.k):
         valid_start, valid_end = i * ratio, (i + 1) * ratio
         valid_folds = folds[valid_start:valid_end]
         train_folds = tuple(set(folds) - (set(test_folds) | set(valid_folds)))
-
         csv_path = output_dir / f'{i}.csv'
         logging.info(f'Write the data split file to "{csv_path.resolve()}".')
         with open(csv_path, 'w', newline='') as f:
             writer = csv.writer(f)
             writer.writerow(['path', 'type'])
             for path in sorted(train_folds):
-                path = args.resampled_data_dir / 'training' / path.name
+                path = args.resampled_data_dir.resolve() / 'training' / path.name
                 writer.writerow([path, 'train'])
             for path in sorted(valid_folds):
-                writer.writerow([path, 'valid'])
+                writer.writerow([path.resolve(), 'valid'])
             for path in sorted(test_folds):
-                writer.writerow([path, 'test'])
+                writer.writerow([path.resolve(), 'test'])
 
     # Write testing data split file.
     patient_dirs = sorted(dir_ for dir_ in (args.data_dir / 'testing').iterdir() if dir_.is_dir())
@@ -51,8 +50,9 @@ def _parse_args():
     parser.add_argument('data_dir', type=Path, help='The directory of the data.')
     parser.add_argument('resampled_data_dir', type=Path, help='The directory of the resampled data.')
     parser.add_argument('output_dir', type=Path, help='The output directory of the data split files.')
-    parser.add_argument('--k', type=int, choices=[5], default=5,
+    parser.add_argument('--k', type=int, choices=[3, 5], default=5,
                         help='The number of folds for cross-validation.')
+    parser.add_argument('--test_size', type=int, default=41, help='The number of testing data.')
     args = parser.parse_args()
     return args
 

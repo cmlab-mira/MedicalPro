3e5594d797b1bb562e61c714fda9698ca4bbbcf5
peter850706 <peter850706@gmail.com>
2020-06-11 18:03:40 +0800

    Update monitor target

--- src/runner/trainers/brats17_seg_trainer.py
+++ src/runner/trainers/brats17_seg_trainer.py
@@ -37,38 +37,17 @@ class Brats17SegTrainer(BaseTrainer):
         loss = (self.loss_weights.cross_entropy_loss * cross_entropy_loss +
                 self.loss_weights.dice_loss * dice_loss)
 
-        # Whole tumor: label 1, 2, 3
-        # Tumor core: label 1, 3
         # Enhancing tumor: label 3
         output_prob = F.softmax(output, dim=1)
-        output_whole_tumor = self.merge_prob(
-            output_prob,
-            background_indices=[0],
-            foreground_indices=[1, 2, 3]
-        )
-        output_tumor_core = self.merge_prob(
-            output_prob,
-            background_indices=[0, 2],
-            foreground_indices=[1, 3]
-        )
         output_enhancing_tumor = self.merge_prob(
             output_prob,
             background_indices=[0, 1, 2],
             foreground_indices=[3]
         )
-        dice_whole_tumor = self.metric_fns.dice(
-            output_whole_tumor,
-            (target > 0).to(torch.long)
-        )[1]
-        dice_tumor_core = self.metric_fns.dice(
-            output_tumor_core,
-            ((target == 1) | (target == 3)).to(torch.long)
-        )[1]
         dice_enhancing_tumor = self.metric_fns.dice(
             output_enhancing_tumor,
             (target == 3).to(torch.long)
         )[1]
-        dice = (dice_whole_tumor + dice_tumor_core + dice_enhancing_tumor) / 3
         return {
             'loss': loss,
             'losses': {
@@ -76,9 +55,6 @@ class Brats17SegTrainer(BaseTrainer):
                 'DiceLoss': dice_loss
             },
             'metrics': {
-                'Dice': dice,
-                'DiceWholeTumor': dice_whole_tumor,
-                'DiceTumorCore': dice_tumor_core,
                 'DiceEnhancingTumor': dice_enhancing_tumor
             }
         }

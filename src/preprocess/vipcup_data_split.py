bbf61e005f618fbc859400be8273e9e1fdfb43db
vic85821 <vic85821@gmail.com>
2020-06-19 22:22:58 +0800

    Split the data according to the total tumor size of each patient

--- src/preprocess/vipcup_data_split.py
+++ src/preprocess/vipcup_data_split.py
@@ -2,26 +2,45 @@ import argparse
 import csv
 import logging
 import random
+import numpy as np
+import nibabel as nib
 from pathlib import Path
 
 
 def main(args):
     # Randomly split the data into k folds.
-    patient_dirs = sorted(dir_ for dir_ in (args.data_dir / 'training').iterdir() if dir_.is_dir())
+    patient_dirs = sorted(dir_ for dir_ in (args.resampled_data_dir / 'training').iterdir() if dir_.is_dir())
 
     # Testing fold, which is the official validation fold
     test_size = args.test_size
     test_folds = patient_dirs[:test_size]
+    folds = patient_dirs[test_size:]
 
-    random.seed(0)
-    folds = random.sample(patient_dirs[test_size:], k=len(patient_dirs[test_size:]))
+    np.random.seed(0)
     output_dir = args.output_dir
     if not output_dir.is_dir():
         output_dir.mkdir(parents=True)
-    ratio = len(folds) // args.k
+
+    # Calculate the total tumor size of each patient
+    patient_info = {}
+    for dir_ in folds:
+        patient_id = dir_.name
+        label_path = dir_ / f'{patient_id}_label.nii.gz'
+        metadata = nib.load(label_path)
+        pixel_size = np.prod(metadata.header['pixdim'][1:4])
+        label = metadata.get_fdata()
+        tumor_size = np.sum(label[label > 0]) * pixel_size
+        patient_info[patient_id] = tumor_size
+
+    # Randomly shuffle the splited patient group
+    sorted_patient_list = sorted(patient_info.keys(), key=patient_info.__getitem__)
+    sorted_patient_list = sorted_patient_list[:-(len(sorted_patient_list) % args.k)]
+    splited_patient_list = np.array(sorted_patient_list).reshape(-1, args.k)
+    np.take(splited_patient_list, np.random.permutation(splited_patient_list.shape[1]), axis=1, out=splited_patient_list)
+    
     for i in range(args.k):
-        valid_start, valid_end = i * ratio, (i + 1) * ratio
-        valid_folds = folds[valid_start:valid_end]
+        valid_patient_list = splited_patient_list[:, i].reshape(-1)
+        valid_folds = [args.resampled_data_dir / 'training' / pid for pid in valid_patient_list]
         train_folds = tuple(set(folds) - (set(test_folds) | set(valid_folds)))
         csv_path = output_dir / f'{i}.csv'
         logging.info(f'Write the data split file to "{csv_path.resolve()}".')
@@ -29,25 +48,11 @@ def main(args):
             writer = csv.writer(f)
             writer.writerow(['path', 'type'])
             for path in sorted(train_folds):
-                path = args.resampled_data_dir.resolve() / 'training' / path.name
-                writer.writerow([path, 'train'])
+                writer.writerow([path.resolve(), 'train'])
             for path in sorted(valid_folds):
                 writer.writerow([path.resolve(), 'valid'])
             for path in sorted(test_folds):
                 writer.writerow([path.resolve(), 'test'])
-
-    # Write adaptation split file
-    csv_path = output_dir / 'adaptation.csv'
-    train_folds = tuple(set(folds) - (set(test_folds)))
-    logging.info(f'Write the data split file to "{csv_path.resolve()}".')
-    with open(csv_path, 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(['path', 'type'])
-        for path in sorted(train_folds):
-            path = args.resampled_data_dir.resolve() / 'training' / path.name
-            writer.writerow([path, 'train'])
-        for path in sorted(test_folds):
-            writer.writerow([path.resolve(), 'test'])
 
     # Write testing data split file.
     patient_dirs = sorted(dir_ for dir_ in (args.data_dir / 'testing').iterdir() if dir_.is_dir())

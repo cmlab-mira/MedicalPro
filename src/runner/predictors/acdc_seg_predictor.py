5ecddb6647cc5d9c326c62d898ebcd79492c1a03
peter850706 <peter850706@gmail.com>
2020-06-21 20:40:30 +0800

    Add gamma percentage

--- src/runner/predictors/acdc_seg_predictor.py
+++ src/runner/predictors/acdc_seg_predictor.py
@@ -1,44 +1,50 @@
+import logging
 import torch
-import numpy as np
 import nibabel as nib
 import matplotlib.pyplot as plt
 import torch.nn.functional as F
 
 from src.runner.predictors import BasePredictor
+from src.runner.predictors.utils import clip_gamma, get_gamma_percentange
+
+LOGGER = logging.getLogger(__name__.split('.')[-1])
 
 
 class AcdcSegPredictor(BasePredictor):
     """The ACDC predictor for the segmentation task.
     Args:
         saved_pred (bool): Whether to save the prediction (default: False).
+        plot_gamma_performance_curve (bool): Whether to plot gamma vs performance figure (default: False).
+        gamma_thresholds (sequence): The thresholds to clip gamma value (default: None).
+            Note that this argument is only valid when plot_gamma_performance_curve is True.
     """
 
-    def __init__(self, saved_pred=False, plot_gamma_performance_curve=False, **kwargs):
+    def __init__(self, saved_pred=False, plot_gamma_performance_curve=False, gamma_thresholds=None, **kwargs):
         super().__init__(**kwargs)
         if self.test_dataloader.batch_size != 1:
             raise ValueError(f'The testing batch size should be 1. Got {self.test_dataloader.batch_size}.')
 
         self.saved_pred = saved_pred
-        self.plot_gamma_performance_curve = plot_gamma_performance_curve
         self.output_dir = self.saved_dir / 'prediction'
         if not self.output_dir.is_dir():
             self.output_dir.mkdir(parents=True)
+        self.plot_gamma_performance_curve = plot_gamma_performance_curve
+        gamma_thresholds = set(gamma_thresholds)
+        gamma_thresholds.add(0)
+        self.gamma_thresholds = sorted(gamma_thresholds)
 
     def predict(self):
         if self.plot_gamma_performance_curve is False:
             super().predict()
         else:
             state_dict = self.net.state_dict()
-            gamma_thresholds = np.arange(0, 1.0, 0.05)
             test_logs = {}
-            for gamma_threshold in gamma_thresholds:
-                tmp_state_dict = state_dict.copy()
-                for key in tmp_state_dict.keys():
-                    if ('norm' in key) and ('weight' in key):
-                        zeros = torch.zeros_like(tmp_state_dict[key])
-                        tmp_state_dict[key] = torch.where(tmp_state_dict[key].abs() <= gamma_threshold, 
-                                                          zeros, tmp_state_dict[key])
+            percentages = []
+            for gamma_threshold in self.gamma_thresholds:
+                tmp_state_dict = clip_gamma(state_dict, gamma_threshold)
                 self.net.load_state_dict(tmp_state_dict)
+                print()
+                LOGGER.info(f'Gamma threshold: {gamma_threshold}')
                 test_log = super().predict()
                 if test_logs:
                     for key in test_log.keys():
@@ -46,14 +52,28 @@ class AcdcSegPredictor(BasePredictor):
                 else:
                     for key in test_log.keys():
                         test_logs[key] = [test_log[key]]
+                percentage = get_gamma_percentange(state_dict, gamma_threshold)
+                percentages.append(f'{percentage: .2f}%')
             for key in test_logs.keys():
-                figure_path = self.saved_dir / f'{key}.png'
                 fig = plt.figure(figsize=(10, 8))
-                plt.plot(gamma_thresholds, test_logs[key])
-                plt.xlabel('gamma threshold')
+                plt.axhline(y=test_logs[key][0], label='No cliping', color='black', linestyle='--')
+                plt.plot(self.gamma_thresholds[1:], test_logs[key][1:], color='blue', marker='o')
+                plt.xscale('log')
+                plt.yscale('log')
+                for i, percentage in enumerate(percentages[1:], start=1):
+                    plt.annotate(
+                        percentage,
+                        xy=(self.gamma_thresholds[i], test_logs[key][i]),
+                        xytext=(-15, -15),
+                        textcoords='offset points',
+                        ha='center'
+                    )
+                plt.xlabel('Gamma threshold')
                 plt.ylabel(key)
+                plt.legend(loc='lower left')
+                figure_path = self.saved_dir / f'{key}.png'
                 fig.savefig(figure_path.as_posix())
-            
+
     def _test_step(self, batch):
         if self.test_dataloader.dataset.csv_name == 'testing.csv':
             input, target = batch['input'].to(self.device), batch['target']
@@ -73,8 +93,8 @@ class AcdcSegPredictor(BasePredictor):
                                    align_corners=False)
             cross_entropy_loss = self.loss_fns.cross_entropy_loss(output, target.squeeze(dim=1))
             dice_loss = self.loss_fns.dice_loss(output, target)
-            loss = (self.loss_weights.cross_entropy_loss * cross_entropy_loss
-                    + self.loss_weights.dice_loss * dice_loss)
+            loss = (self.loss_weights.cross_entropy_loss * cross_entropy_loss +
+                    self.loss_weights.dice_loss * dice_loss)
             dice = self.metric_fns.dice(F.softmax(output, dim=1), target)
 
         if self.saved_pred:
